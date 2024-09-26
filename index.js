da18c6f99174624756b05deb58f38df3317d8181
Aman Kumar <amankumaroo784@gmail.com>
2024-09-26 10:14:42 +0530

    project setup intial

---
+++ index.js
@@ -0,0 +1,21 @@
+import path from "path";
+import express from "express";
+
+const port = 3001;
+const app = express();
+
+import adminRoutes from "./routes/admin.route.js";
+import shopRoutes from "./routes/shop.route.js";
+
+app.use(express.static("public"));
+
+app.use("/admin", adminRoutes);
+app.use("/", shopRoutes);
+
+app.use((req, res) => {
+  res.sendFile(path.join(path.resolve(), "views", "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
